0d93767e837806f8ae8da0bb9d054704f11233d5
alexislopes <alexisdrops@gmail.com>
2020-04-20 18:47:56 -0300

    chore(rota inicial): adiciona rota inicial

--- routes/informe.js
+++ routes/informe.js
@@ -1,6 +1,10 @@
 const { findAll } = require("../services/informeServices");
 
 module.exports = (app) => {
+  app.get("/", async (req, res) => {
+    res.json({ message: "Working" });
+  });
+
   app.get("/informe", async (req, res) => {
     const informes = await findAll();
     res.status(201).json({
