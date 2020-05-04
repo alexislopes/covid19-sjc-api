4cbeb05c74537f73f7d3dd011ec6be725f64e5fb
alexislopes <alexisdrops@gmail.com>
2020-05-04 18:10:39 -0300

    feat(sort): aplica ordenação no retorno da busca de informes

--- services/informeServices.js
+++ services/informeServices.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const Informe = mongoose.model("Informe");
 
 async function findAll() {
-  return await Informe.find({});
+  const informes = await Informe.find({});
+
+  return await informes.sort((a, b) => (a.timestamp < b.timestamp ? 1 : -1));
 }
 
 async function create(informe) {
